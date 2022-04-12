65eae82c0b233d5606c5094921e105a55ab0c63a
cartwheee11 <cartwheel@MBP-Cartwheel.Dlink>
2022-04-12 14:03:39 +0300

    more cats

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -65,305 +65,309 @@
 </template>
 
 <script>
-const Masonry = require("masonry-layout");
+  const Masonry = require("masonry-layout");
 
-import ImagePreview from "@/components/ImagePreview.vue";
+  import ImagePreview from "@/components/ImagePreview.vue";
 
-export default {
-  name: "Home",
-  components: {
-    ImagePreview,
-  },
-
-  data() {
-    return {
-      masonry: null,
-      imagesInChunk: 30,
-      loadedImages: [],
-      currentChunk: [],
-      fileList: [],
-      imageClickStatus: "",
-      favourites: [],
-    };
-  },
-
-  created() {
-    document.addEventListener("scroll", this.onScrolledDownHandler);
-  },
-
-  mounted() {
-    this.favourites =
-      JSON.parse(window.localStorage.getItem("favorites")) || [];
-    fetch("cats/fileList.json").then(async (response) => {
-      this.fileList = await response.json();
-    });
-  },
-
-  watch: {
-    fileList() {
-      if (this.fileList.length) {
-        this.loadChunk();
-      }
+  export default {
+    name: "Home",
+    components: {
+      ImagePreview,
     },
 
-    favourites() {
-      if (!this.favourites.length) {
-        this.$router.push({ name: "Home" });
-      }
+    data() {
+      return {
+        masonry: null,
+        imagesInChunk: 30,
+        loadedImages: [],
+        currentChunk: [],
+        fileList: [],
+        imageClickStatus: "",
+        favourites: [],
+      };
+    },
 
-      if (this.$route.path == "/favorites") {
+    created() {
+      document.addEventListener("scroll", this.onScrolledDownHandler);
+    },
+
+    mounted() {
+      this.favourites =
+        JSON.parse(window.localStorage.getItem("favorites")) || [];
+      fetch("cats/fileList.json").then(async (response) => {
+        this.fileList = await response.json();
+      });
+    },
+
+    watch: {
+      fileList() {
+        if (this.fileList.length) {
+          this.loadChunk();
+        }
+      },
+
+      favourites() {
+        if (!this.favourites.length) {
+          this.$router.push({ name: "Home" });
+        }
+
+        if (this.$route.path == "/favorites") {
+          this.$nextTick(() => {
+            this.onImageLoad();
+          });
+        }
+      },
+
+      $route() {
         this.$nextTick(() => {
           this.onImageLoad();
         });
-      }
+      },
     },
 
-    $route() {
-      this.$nextTick(() => {
-        this.onImageLoad();
-      });
-    },
-  },
-
-  methods: {
-    switchFav(key) {
-      let i = this.favourites.indexOf(key);
-      if (i != -1) {
-        this.favourites = this.favourites
-          .slice(0, i)
-          .concat(this.favourites.slice(i + 1, this.favourites.length));
-      } else {
-        this.favourites.push(key);
-      }
-
-      window.localStorage.setItem("favorites", JSON.stringify(this.favourites));
-    },
-
-    onImageMouseUp() {},
-
-    onImageClick(name) {
-      navigator.clipboard.writeText("https://cats.cartwheel.top/cats/" + name);
-      this.imageClickStatus = true;
-    },
-
-    onScrolledDownHandler() {
-      let docHeight = document.body.clientHeight;
-      let scroll = window.scrollY + window.innerHeight;
-      if (scroll >= docHeight) {
-        this.loadChunk();
-      }
-    },
-
-    onImageLoad() {
-      this.masonry = new Masonry(this.$refs.feed, {
-        itemSelector: ".cat-image-wrapper",
-        fitWidth: true,
-        containerStyle: { width: "100%" },
-        percentPosition: true,
-        transitionDuration: "0s",
-      });
-      this.$refs.feed.style.width = this.$refs.feed.width;
-      this.$refs.feed.style.width = "100%";
-    },
-
-    loadChunk() {
-      for (let i = 0; i < this.imagesInChunk; i++) {
-        if (this.fileList.length) {
-          this.loadedImages.push(this.fileList.shift());
+    methods: {
+      switchFav(key) {
+        let i = this.favourites.indexOf(key);
+        if (i != -1) {
+          this.favourites = this.favourites
+            .slice(0, i)
+            .concat(this.favourites.slice(i + 1, this.favourites.length));
         } else {
-          break;
+          this.favourites.push(key);
         }
-      }
 
-      // this.currentChunk.forEach(name => {
-      //   fetch('https://cats.cartwheel.top/small/' + name).then(res => res.body).then( async body => {
-      //     let reader = body.getReader();
-      //     let done;
-      //     while (!done) {
-      //       ({
-      //         done
-      //       } = await reader.read());
-      //       if (done) {
-      //         this.loadedImages = this.loadedImages.concat([ name ]);
-      //         if((this.loadedImages.length % this.imagesInChunk) == 0) {
-      //           document.addEventListener('scroll', this.onScrolledDownHandler);
-      //         }
-      //       }
-      //     }
-      //   })
-      // })
+        window.localStorage.setItem(
+          "favorites",
+          JSON.stringify(this.favourites)
+        );
+      },
 
-      // this.currentChunk = [];
+      onImageMouseUp() {},
+
+      onImageClick(name) {
+        navigator.clipboard.writeText(
+          "https://cats.cartwheel.top/cats/" + name
+        );
+        this.imageClickStatus = true;
+      },
+
+      onScrolledDownHandler() {
+        let docHeight = document.body.clientHeight;
+        let scroll = window.scrollY + window.innerHeight;
+        if (scroll >= docHeight) {
+          this.loadChunk();
+        }
+      },
+
+      onImageLoad() {
+        this.masonry = new Masonry(this.$refs.feed, {
+          itemSelector: ".cat-image-wrapper",
+          fitWidth: true,
+          containerStyle: { width: "100%" },
+          percentPosition: true,
+          transitionDuration: "0s",
+        });
+        this.$refs.feed.style.width = this.$refs.feed.width;
+        this.$refs.feed.style.width = "100%";
+      },
+
+      loadChunk() {
+        for (let i = 0; i < this.imagesInChunk; i++) {
+          if (this.fileList.length) {
+            this.loadedImages.push(this.fileList.shift());
+          } else {
+            break;
+          }
+        }
+
+        // this.currentChunk.forEach(name => {
+        //   fetch('https://cats.cartwheel.top/small/' + name).then(res => res.body).then( async body => {
+        //     let reader = body.getReader();
+        //     let done;
+        //     while (!done) {
+        //       ({
+        //         done
+        //       } = await reader.read());
+        //       if (done) {
+        //         this.loadedImages = this.loadedImages.concat([ name ]);
+        //         if((this.loadedImages.length % this.imagesInChunk) == 0) {
+        //           document.addEventListener('scroll', this.onScrolledDownHandler);
+        //         }
+        //       }
+        //     }
+        //   })
+        // })
+
+        // this.currentChunk = [];
+      },
     },
-  },
-};
+  };
 </script>
 
-
 <style scoped>
-@keyframes show {
-  from {
-    transform: translate(0, 20px);
-    opacity: 0;
+  @keyframes show {
+    from {
+      transform: translate(0, 20px);
+      opacity: 0;
+    }
+
+    50% {
+      transform: translate(0, 20px);
+      opacity: 0;
+    }
+
+    to {
+      transform: translate(0, 0px);
+      opacity: 1;
+    }
   }
 
-  50% {
-    transform: translate(0, 20px);
-    opacity: 0;
+  @keyframes spin {
+    from {
+      transform: rotate(0);
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
   }
 
-  to {
-    transform: translate(0, 0px);
-    opacity: 1;
-  }
-}
-
-@keyframes spin {
-  from {
-    transform: rotate(0);
+  h1 {
+    font-family: f2p;
+    font-weight: 100;
+    font-size: 40px;
+    line-height: 1.4em;
   }
 
-  to {
-    transform: rotate(360deg);
+  p {
+    font-family: mine;
+    font-size: 20px;
   }
-}
 
-h1 {
-  font-family: f2p;
-  font-weight: 100;
-  font-size: 40px;
-  line-height: 1.4em;
-}
+  a {
+    color: black;
+    text-decoration: none;
+  }
 
-p {
-  font-family: mine;
-  font-size: 20px;
-}
+  .filter a {
+    /* margin-right: 20px; */
+    margin: 0 10px;
+    padding-bottom: 5px;
+  }
 
-a {
-  color: black;
-  text-decoration: none;
-}
+  .filter a:hover {
+    border-bottom: 4px #aaa solid;
+  }
 
-.filter a {
-  /* margin-right: 20px; */
-  margin: 0 10px;
-  padding-bottom: 5px;
-}
+  .filter a:active,
+  .filter .router-link-active {
+    border-bottom: 4px black solid;
+  }
 
-.filter a:hover {
-  border-bottom: 4px #aaa solid;
-}
-
-.filter a:active,
-.filter .router-link-active {
-  border-bottom: 4px black solid;
-}
-
-.spinner {
-  animation-name: spin;
-  animation-duration: 1s;
-  /* opacity: 0.5; */
-  animation-iteration-count: infinite;
-}
-
-.feed-wrapper {
-  /* width: 1000px; */
-  max-width: 1000px;
-  margin: 0 auto;
-}
-
-.container {
-  /* width: 1000px; */
-  max-width: 1000px;
-
-  margin: 0 auto;
-  text-align: center;
-}
-
-.feed {
-  width: 100%;
-  position: relative;
-}
-
-.header-container {
-  padding: 0 50px;
-  /* width: 600px;  */
-  margin-top: 100px;
-  margin-bottom: 100px;
-}
-
-.header-container img {
-  /* margin-bottom: 10px; */
-}
-
-.header-container h1 {
-  margin-top: 10px;
-}
-
-.footer {
-  padding: 50px 0;
-}
-
-@media screen and (max-width: 1000px) {
-  .cat-image-wrapper {
-    width: 50%;
+  .spinner {
+    animation-name: spin;
+    animation-duration: 1s;
+    /* opacity: 0.5; */
+    animation-iteration-count: infinite;
   }
 
   .feed-wrapper {
-    /* max-width: 1000px; */
-  }
-}
-
-@media screen and (max-width: 760px) {
-  .header-container h1 {
-    font-size: 30px;
+    /* width: 1000px; */
+    max-width: 1000px;
+    margin: 0 auto;
   }
 
-  .header-container {
-    padding: 0 10px !important;
-    margin: 50px 0;
+  .container {
+    /* width: 1000px; */
+    max-width: 1000px;
+
+    margin: 0 auto;
+    text-align: center;
   }
 
-  .header-container img {
-    width: 70px;
-  }
-
-  p,
-  a {
-    font-size: 16px;
-  }
-
-  .cat-image-wrapper {
+  .feed {
     width: 100%;
-    padding: 10px;
-  }
-}
-
-@media screen and (max-width: 500px) {
-  * {
-    text-align: left;
-  }
-
-  .header-icon {
-    display: none;
-  }
-
-  .header-container h1 {
-    font-size: 25px;
+    position: relative;
   }
 
   .header-container {
-    margin-top: 25px;
+    padding: 0 50px;
+    /* width: 600px;  */
+    margin-top: 100px;
+    margin-bottom: 100px;
   }
 
   .header-container img {
-    width: 50px;
+    /* margin-bottom: 10px; */
   }
 
-  p,
-  a {
-    font-size: 11px;
+  .header-container h1 {
+    margin-top: 10px;
   }
-}
+
+  .footer {
+    padding: 50px 0;
+  }
+
+  @media screen and (max-width: 1000px) {
+    .cat-image-wrapper {
+      width: 50%;
+    }
+
+    .feed-wrapper {
+      /* max-width: 1000px; */
+    }
+  }
+
+  @media screen and (max-width: 760px) {
+    .header-container h1 {
+      font-size: 30px;
+    }
+
+    .header-container {
+      padding: 0 10px !important;
+      margin: 50px 0;
+    }
+
+    .header-container img {
+      width: 70px;
+    }
+
+    p,
+    a {
+      font-size: 16px;
+    }
+
+    .cat-image-wrapper {
+      width: 100%;
+      padding: 10px;
+    }
+  }
+
+  @media screen and (max-width: 500px) {
+    * {
+      text-align: left;
+    }
+
+    .header-icon {
+      display: none;
+    }
+
+    .header-container h1 {
+      font-size: 25px;
+    }
+
+    .header-container {
+      margin-top: 25px;
+    }
+
+    .header-container img {
+      width: 50px;
+    }
+
+    p,
+    a {
+      font-size: 11px;
+    }
+  }
 </style>
